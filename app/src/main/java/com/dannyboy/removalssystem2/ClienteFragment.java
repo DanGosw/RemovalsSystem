b5da299f808dfdde39d8b2262368c6c85b1c91e0
Danny Boy <dannydospuntosv@gmail.com>
2022-12-13 18:19:28 -0500

    Initial Commit

--- app/src/main/java/com/dannyboy/removalssystem2/ClienteFragment.java
+++ app/src/main/java/com/dannyboy/removalssystem2/ClienteFragment.java
@@ -1,61 +1,72 @@
 package com.dannyboy.removalssystem2;
 
 	import androidx.fragment.app.Fragment;
+	import android.media.Ringtone;
+	import android.media.RingtoneManager;
+	import android.net.Uri;
 	import android.os.Bundle;
-	import android.os.StrictMode;
+	import android.text.InputFilter;
 	import android.view.LayoutInflater;
 	import android.view.View;
 	import android.view.ViewGroup;
-	import android.widget.Button;
 	import android.widget.EditText;
 	import android.widget.ImageButton;
 	import android.widget.Toast;
-	
 	import com.android.volley.Request;
-	import com.android.volley.Response;
-	import com.android.volley.VolleyError;
 	import com.android.volley.toolbox.StringRequest;
-	
 	import org.json.JSONException;
 	import org.json.JSONObject;
-	import org.jsoup.Connection;
-
 
 public class ClienteFragment extends Fragment {
 
 View vista;
 EditText dni, nom, pat, mat, cel;
 ImageButton cons;
+StringRequest postRequest;
 
-private String api = "";
+
 @Override
 public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 	
 	 vista = inflater.inflate(R.layout.activity_cliente_fragment, container, false);
-	String a_principal = "https://api.sunat.cloud/ruc/20454910215";
-	api = a_principal + "persons/";
+
 	 dni = vista.findViewById(R.id.txt_dni);
 	 nom = vista.findViewById(R.id.txt_nom);
 	 pat = vista.findViewById(R.id.txt_pat);
 	 mat = vista.findViewById(R.id.txt_mat);
 	 cel = vista.findViewById(R.id.txt_cel);
 	 cons = vista.findViewById(R.id.search_customer);
+	 dni.setFilters(new InputFilter[]{new InputFilter.LengthFilter(8)} );
 	 
-	 cons.setOnClickListener(view -> ConsumeApi());
+	 cons.setOnClickListener(view -> {
+		if (dni.getText().length() != 8){
+			Toast.makeText(getContext(), "Dni no valido", Toast.LENGTH_SHORT).show();
+			return;
+		}
+		ConsumeApi();
+	 });
 	return  vista;
 }
 
 public void ConsumeApi() {
-	StringRequest postRequest = new StringRequest(Request.Method.GET, api + dni.getText(), response -> {
+	String a_principal = "https://apiperu.dev/api/dni/";
+	String token = "k4d2956bd531ab61d44f4fa07304b20e13913815";
+	postRequest = new StringRequest(Request.Method.GET, a_principal  +dni.getText()+"?api_token="+ token, response -> {
 		
 		try {
-			JSONObject jsonObject = new JSONObject(response.toString());
-			nom.setText(jsonObject.getString("name"));
+			JSONObject jsonObject = new JSONObject(response);
+			nom.setText(jsonObject.getString("name"+ "n"));
 			pat.setText(jsonObject.getString("first_name"));
 			mat.setText(jsonObject.getString("last_name"));
+			
+			Uri uri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+			Ringtone r = RingtoneManager.getRingtone(getContext(), uri);
+			r.play();
+			
 		} catch (JSONException ignored) {
 			Toast.makeText(getContext(), "Error en la consulta", Toast.LENGTH_SHORT).show();
 		}
 	}, error -> Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show());
+
 }
 }
--- app/src/main/java/com/dannyboy/removalssystem2/HomeFragment.java
+++ app/src/main/java/com/dannyboy/removalssystem2/HomeFragment.java
@@ -1,29 +1,56 @@
 package com.dannyboy.removalssystem2;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.location.FusedLocationProviderClient;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class HomeFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener{
 
 MapView mMapView;
 GoogleMap mMap;
+FusedLocationProviderClient fusedLocationProviderClient;
 View vista;
-TextView lat, lon;
+TextView lat;
+Location loc;
+TextView lon;
+
+@SuppressLint("StaticFieldLeak")
+static TextView ubi;
 @Override
 public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 	vista = inflater.inflate(R.layout.activity_home_fragment, container, false);
-	
 	return vista;
 }
 @Override
@@ -34,12 +61,17 @@ public void onViewCreated (@NonNull View view, Bundle savedInstanceState) {
 	mMapView = vista.findViewById(R.id.map);
 	lat = vista.findViewById(R.id.latit);
 	lon = vista.findViewById(R.id.longi);
+	ubi = vista.findViewById(R.id.ubi);
+	fusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(requireContext());
 	if (mMapView != null) {
 		mMapView.onCreate(null);
 		mMapView.onResume();
 		mMapView.getMapAsync(this);
 	}
+	if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+		ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
 	}
+}
 @Override
 public void onMapReady(@NonNull GoogleMap googleMap) {
 	mMap = googleMap;
@@ -47,8 +79,9 @@ public void onMapReady(@NonNull GoogleMap googleMap) {
 	this.mMap.setOnMapLongClickListener(this);
 	
 	LatLng pe = new LatLng(-9.125574839157506, -78.51786661473645);
-	mMap.addMarker(new MarkerOptions().position(pe).title("Mi ubicacion"));
+	mMap.addMarker(new MarkerOptions().position(pe).title("Aqui vivo :D"));
 	mMap.moveCamera(CameraUpdateFactory.newLatLng(pe));
+
 }
 
 @Override
@@ -60,6 +93,14 @@ public void onMapClick(@NonNull LatLng latLng) {
 	LatLng mexico = new LatLng(latLng.latitude,latLng.longitude);
 	mMap.addMarker(new MarkerOptions().position(mexico).title(""));
 	mMap.moveCamera(CameraUpdateFactory.newLatLng(mexico));
+	Geocoder geocoder = new Geocoder(requireContext(), Locale.getDefault());
+	List <Address> direct;
+	try {
+		direct = geocoder.getFromLocation(latLng.latitude, latLng.longitude,1);
+		ubi.setText(direct.get(0).getAddressLine(0));
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
 }
 
 @Override
@@ -70,5 +111,15 @@ public void onMapLongClick(@NonNull LatLng latLng) {
 	LatLng mexico = new LatLng(latLng.latitude,latLng.longitude);
 	mMap.addMarker(new MarkerOptions().position(mexico).title(""));
 	mMap.moveCamera(CameraUpdateFactory.newLatLng(mexico));
+	
+	Geocoder geocoder = new Geocoder(requireContext(), Locale.getDefault());
+	List <Address> direct;
+	try {
+		direct = geocoder.getFromLocation(latLng.latitude, latLng.longitude,1);
+		ubi.setText(direct.get(0).getAddressLine(0));
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
 }
+
 }
